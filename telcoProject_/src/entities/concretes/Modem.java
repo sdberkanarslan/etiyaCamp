0436e1af0b91f1bfb7c74746c61c0b98796c5cd1
sdberkanarslan <109750892+sdberkanarslan@users.noreply.github.com>
2022-07-27 00:26:10 +0300

    telcoProject

---
+++ telcoProject_/src/entities/concretes/Modem.java
@@ -0,0 +1,19 @@
+package entities.concretes;
+
+public class Modem extends Product {
+
+    private String modemType;
+
+    public Modem(int id, Cable cable, Brand brand, String serialNumber) {
+        super(id, cable, brand, serialNumber);
+    }
+
+
+    public String getModemType() {
+        return modemType;
+    }
+
+    public void setModemType(String modemType) {
+        this.modemType = modemType;
+    }
+}
